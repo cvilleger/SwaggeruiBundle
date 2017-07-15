cb02d8d5633c848b729e10d8d91ec68f4d0721f2
cvilleger <villeger.c@gmail.com>
2017-07-15 04:24:46 +0200

    :wrench: Parameters title and json added

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -8,6 +8,23 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('CvillegerSwaggeruiBundle:Default:index.html.twig');
+        $parameterName = 'swaggerui';
+        $parameters = null;
+        if ($this->container->hasParameter($parameterName)){
+            /**
+             * @var array $swaggerui
+             * example: [
+             *     title: 'SwaggeruiBundleProject',
+             *     url: 'http://petstore.swagger.io/v2/swagger.json'
+             * ]
+             */
+            $swaggerui = $this->container->getParameter($parameterName);
+            foreach ($swaggerui as $value){
+                $key = key($value);
+                $parameters[$key] = $value[$key];
+            }
+        }
+
+        return $this->render('CvillegerSwaggeruiBundle:Default:index.html.twig', $parameters);
     }
 }
